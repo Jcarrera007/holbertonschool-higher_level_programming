b8d337f616d0633e94e38c9f495e48d4dfcb9347
Jcarrera007 <carrerasproductions@gmail.com>
2025-05-08 19:39:47 -0400

    fix the pycodestyle issue

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print("{}".format("".join(chr(c) for c in range(97, 123) if c != 113 and c != 101)), end="")
+print("{}".format("".join(chr(c) for c in range(97, 123) if c != 113 and c != 101)
+), end="")
