aa4fd98e608219b411dd7e165e8289e9305e36eb
Jcarrera007 <carrerasproductions@gmail.com>
2025-05-09 11:33:32 -0400

    ..

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
-    if not isinstance(str, str):
-        raise TypeError("Expected a string")
     result = ""
     for char in str:
         if 'a' <= char <= 'z':
