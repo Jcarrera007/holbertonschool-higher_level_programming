637a5c08b2f6da32bf71a66c44aebf1b6783b84d
Jcarrera007 <carrerasproductions@gmail.com>
2025-05-10 19:01:19 -0400

    update on the code and fix the calc issue

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 # Import functions from calculator_1.py
-from calculator_1 import add, sub, mul, div
+from calculator_1 import calc 
 
 if __name__ == "__main__":
     a = 10
     b = 5
 
     # Perform calculations
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+    print("{} + {} = {}".format(a, b, calc.add(a, b)))
+    print("{} - {} = {}".format(a, b, calc.sub(a, b)))
+    print("{} * {} = {}".format(a, b, calc.mul(a, b)))
+    print("{} / {} = {}".format(a, b, calc.div(a, b)))
