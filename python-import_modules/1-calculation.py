809c4f4300977eba18aba03d7179ee5ec730b5b4
Jcarrera007 <carrerasproductions@gmail.com>
2025-05-10 18:49:16 -0400

    created the file 1-calculation.py

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# Import functions from calculator_1.py
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+
+    # Perform calculations
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
