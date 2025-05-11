748040e3676770442f1755929977c631ab63de05
Jcarrera007 <carrerasproductions@gmail.com>
2025-05-11 16:22:39 -0400

    fixed the issue

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,20 +3,19 @@
 import sys
 
 def main():
-    argv = sys.argv[1:]  
-    num_args = len(argv)
+    num_args = len(sys.argv) - 1
 
     
     if num_args == 0:
-        print("0 argument")
+        print("0 argument:")
     elif num_args == 1:
         print("1 argument:")
     else:
         print(f"{num_args} arguments:")
 
     
-    for i in range(num_args):
-        print(f"{i + 1}: {argv[i]}")
+    for i in range(1, len(sys.argv)):
+        print(f"{i}: {sys.argv[i]}")
 
 if __name__ == "__main__":
     main()
