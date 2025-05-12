d9aa18d78e55325348f3da2af0b69143e82401b8
Jcarrera007 <carrerasproductions@gmail.com>
2025-05-12 16:36:36 -0400

    corrected some issues with the code

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if char in my_string:
-        return my_string.replace('c', '').replace('C', '')
-    return my_string
+    for char in my_string:
+        if char == 'c' or char == 'C':
+            return my_string.replace('c', '').replace('C', '')
