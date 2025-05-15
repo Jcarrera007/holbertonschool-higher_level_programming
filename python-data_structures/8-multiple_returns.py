028e69c14d337f8f093530436fe1f754bdb9c126
Jcarrera007 <carrerasproductions@gmail.com>
2025-05-14 23:38:30 -0400

    change the typo on the code itself

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b=()):
+def multiple_returns(sentence):
 
-    tuple_a = tuple_a + (0, 0)
-    tuple_b = tuple_b + (0, 0)
-
-    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    if sentence == "":
+        return (0, None)
+    else:
+        return (len(sentence), sentence[0])
