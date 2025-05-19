a6b3746314142a6455cf6e80e848116180bcaddd
Jcarrera007 <carrerasproductions@gmail.com>
2025-05-19 15:59:06 -0400

    fix: add validation to ensure all rows in the matrix have the same size

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -31,6 +31,9 @@ def matrix_divided(matrix, div):
         raise TypeError(
             "matrix must be a matrix (list of lists) of integers/floats"
         )
+    row_lengths = [len(row) for row in matrix]
+    if len(set(row_lengths)) > 1:
+        raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
