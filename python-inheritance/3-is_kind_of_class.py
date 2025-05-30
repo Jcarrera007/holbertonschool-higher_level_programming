83e27ae6b695147416e76da0a20cb9b176a53315
Jcarrera007 <carrerasproductions@gmail.com>
2025-05-30 10:16:39 -0400

    fix: add missing newline at the beginning of is_kind_of_class.py

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module for is_kind_of_class function."""
 
+
 def is_kind_of_class(obj, a_class):
     """Checks if an object is an instance of, or if it is an instance of a class
     that inherited from, a specified class.
