60ad3bf27a18be887f1623604e305de50f2e8a91
Jcarrera007 <carrerasproductions@gmail.com>
2025-05-30 11:28:35 -0400

    feat: add BaseGeometry class with area method

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module for BaseGeometry class."""
+
+
+class BaseGeometry:
+    """A class representing basic geometric operations."""
+
+    def area(self):
+        """Raises an exception indicating area is not implemented."""
+        raise Exception("area() is not implemented")
