529cf122726c58c18294e7f179a7f1e25b2f0245
Jcarrera007 <carrerasproductions@gmail.com>
2025-06-09 02:40:29 -0400

    fix: correct module docstring indentation in write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
-
+"""Module for write_file function."""
+ 
+ 
 def write_file(filename="", text=""):
     """Writes a string to a text file (UTF8) and returns the number of characters written."""
     with open(filename, "w", encoding="utf-8") as f:
