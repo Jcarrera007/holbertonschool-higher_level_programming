106df18254918ea5c4b29c874c8bb7385e51904e
Jcarrera007 <carrerasproductions@gmail.com>
2025-06-13 11:17:14 -0400

    refactor: streamline response handling in Server class

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -4,59 +4,56 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 
-class SimpleAPIHandler(BaseHTTPRequestHandler):
-    def _set_headers(self, status_code=200, content_type="application/json"):
-        self.send_response(status_code)
-        self.send_header("Content-Type", content_type)
-        self.end_headers()
-
+class Server(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/":
-            self._set_headers(200, "text/plain")
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
-
         elif self.path == "/data":
-            self._set_headers(200, "application/json")
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
             data = {"name": "John", "age": 30, "city": "New York"}
-            response = json.dumps(data, separators=(",", ":"))
-            self.wfile.write(response.encode("utf-8"))
-
+            self.wfile.write(json.dumps(data, separators=(",", ":")).encode("utf-8"))
         elif self.path == "/status":
-            self._set_headers(200, "text/plain")
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
             self.wfile.write(b"OK")
-
         elif self.path == "/info":
-            self._set_headers(200, "application/json")
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
             info = {"version": "1.0", "description": "A simple API built with http.server"}
-            response = json.dumps(info, separators=(",", ":"))
-            self.wfile.write(response.encode("utf-8"))
-
+            self.wfile.write(json.dumps(info, separators=(",", ":")).encode("utf-8"))
         else:
-            self._set_headers(404, "application/json")
-            error = {"error": "Not found"}
-            response = json.dumps(error, separators=(",", ":"))
-            self.wfile.write(response.encode("utf-8"))
+            self.send_response(404)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Endpoint no encontrado")
 
     def do_POST(self):
         content_length = int(self.headers.get("Content-Length", 0))
         post_data = self.rfile.read(content_length)
-
         try:
             data = json.loads(post_data.decode("utf-8"))
-            self._set_headers(200)
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
             response = {"received": data}
         except json.JSONDecodeError:
-            self._set_headers(400)
+            self.send_response(400)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
             response = {"error": "Invalid JSON"}
-
         self.wfile.write(json.dumps(response, separators=(",", ":")).encode("utf-8"))
 
-
-def run(server_class=HTTPServer, handler_class=SimpleAPIHandler):
+def run(server_class=HTTPServer, handler_class=Server):
     server_address = ('', 8000)
     httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
-
 
 if __name__ == "__main__":
     run()
