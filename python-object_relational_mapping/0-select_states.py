6d19755b935c9c1a164a6aee2571a7fb877ba08c
Jcarrera007 <carrerasproductions@gmail.com>
2025-06-29 22:27:17 -0400

    fix: update password handling in 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
         host="localhost",
         port=3306,
         user=username,
-        passwd=password,
+        passwd="Jcarre007",
         db=database
     )
     
