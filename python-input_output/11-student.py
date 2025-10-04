30665f8775bc57bb4266e947206bcf562e51e73f
Jcarrera007 <carrerasproductions@gmail.com>
2025-10-04 02:01:58 -0400

    Implement file handling functions: read_file, write_file, append_write, to_json_string, from_json_string, save_to_json_file, load_from_json_file; add Student class with JSON serialization methods; create Pascal's triangle function; update README with usage and concepts.

---
+++ python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module for Student class."""
+
+
+class Student:
+    """Defines a student."""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance.
+        If attrs is a list of strings, only those attributes are included.
+        Otherwise, all attributes are included.
+        """
+        if isinstance(attrs, list) and all(isinstance(attr, str)
+                                           for attr in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student
+        instance from a dictionary."""
+        for key, value in json.items():
+            setattr(self, key, value)
